3eb1eaf420540c630b89f8e4d28df03f625f95dc
Utsav Mishra <utsav@Utsavs-MacBook-Pro.local>
2024-02-03 03:55:31 +0530

    added map

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,3 +1,4 @@
 class AppConstants {
   static const String appName = 'Ambularm';
+  static const double mapZoom = 15;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:ambularm/constants/app_constants.dart';
+import 'package:ambularm/utils/location.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 Future<void> main() async {
-  await dotenv.load(fileName: "assets/environments/.env", mergeWith: {
-    'TEST_VAR': '5',
-  });
+  await dotenv.load(fileName: "assets/environments/.env");
   runApp(const MyApp());
 }
 
@@ -16,28 +16,29 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    print(dotenv.env);
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge); // Enable Edge-to-Edge on Android 10+
+    SystemChrome.setEnabledSystemUIMode(
+        SystemUiMode.edgeToEdge); // Enable Edge-to-Edge on Android 10+
 
     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-        systemNavigationBarColor: Colors.transparent, // Setting a transparent navigation bar color
+        systemNavigationBarColor:
+            Colors.transparent, // Setting a transparent navigation bar color
         systemNavigationBarContrastEnforced: true, // Default
-        systemNavigationBarIconBrightness: Brightness.light // This defines the color of the scrim
-    ));
+        systemNavigationBarIconBrightness:
+            Brightness.light, // This defines the color of the scrim
+        statusBarColor: Colors.transparent,
+        systemStatusBarContrastEnforced: true,
+        statusBarIconBrightness: Brightness.light));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: AppConstants.appName,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: const ColorScheme.light()
-      ),
+      theme:
+          ThemeData(useMaterial3: true, colorScheme: const ColorScheme.light()),
       home: const MyHomePage(title: AppConstants.appName),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-
   const MyHomePage({super.key, required this.title});
 
   final String title;
@@ -46,68 +47,51 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  LatLng currentLocation = const LatLng(28.7041, 77.1025);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    print('build');
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: GoogleMap(
+        onMapCreated: _onMapCreation,
+        initialCameraPosition: CameraPosition(
+          target: currentLocation,
+          zoom: AppConstants.mapZoom,
         ),
+        markers: {
+          Marker(
+            markerId: const MarkerId("marker1"),
+            position: currentLocation,
+          ),
+        },
+        minMaxZoomPreference: const MinMaxZoomPreference(11, 19),
+        myLocationButtonEnabled: true,
+        zoomControlsEnabled: false,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
+  Future<void> _onMapCreation(controller) async {
+    try {
+      final location = await determinePosition();
+      currentLocation = LatLng(location.latitude, location.longitude);
+      controller.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(
+            target: currentLocation,
+            zoom: AppConstants.mapZoom,
+          ),
+        ),
+      );
+      setState(() {
+
+      });
+      print(location);
+    } catch (error) {
+      print('error in location determination: $error');
+      //Location determination failed
+    }
+  }
 }
